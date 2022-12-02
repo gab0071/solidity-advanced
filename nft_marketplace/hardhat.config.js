922fc476a4ff01e7b4d0abce2549656cf8a8a12d
catherinee24 <catherinemendez24@gmail.com>
2022-12-01 19:07:05 -0500

    ✨ Update Hardhat

--- nft_marketplace/hardhat.config.js
+++ nft_marketplace/hardhat.config.js
@@ -1,19 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
-  solidity: "0.8.4",
-  paths: {
-    artifacts: "./src/backend/artifacts",
-    sources: "./src/backend/contracts",
-    cache: "./src/backend/cache",
-    tests: "./src/backend/test"
-  },
-  defaultNetwork: "ganache",
-  networks: {
-    ganache: {
-      url: "http://172.21.144.1:7545"
+    solidity: '0.8.4',
+    paths: {
+        artifacts: './src/backend/artifacts',
+        sources: './src/backend/contracts',
+        cache: './src/backend/cache',
+        tests: './src/backend/test',
     },
-    // hardhat: {
-    // },
-  },
+    defaultNetwork: 'ganache',
+    networks: {
+        ganache: {
+            url: 'http://172.29.176.1:7545',
+        },
+        // hardhat: {
+        // },
+    },
 };
